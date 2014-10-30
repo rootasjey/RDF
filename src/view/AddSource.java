b96b83d32ee799e3c0c4a58167d1cca209947af4
angemarie <angemariendri@yahoo.fr>
2014-10-30 17:58:26 +0100

    Revert "Reduction des lignes de code dans la classe AddSource"
    This reverts commit f7f798dfd77f28fe96c106f02a45ae0dcc5a0851.

--- src/view/AddSource.java
+++ src/view/AddSource.java
@@ -171,7 +171,48 @@ public class AddSource extends JDialog {
 	}
 	
 	
+	// Description et extension acceptée par le filtre
+	public class SimpleFilter extends FileFilter{
+		   private String description;
+		   private String extension;
+		   
+		   // Constructeur à partir de la description et de l'extension acceptée
+		   public SimpleFilter(String description, String extension){
+		      if(description == null || extension == null){
+		         throw new NullPointerException("La description (ou extension) ne peut être null.");
+		      }
+		      
+		      this.description = description;
+		      this.extension = extension;
+		   }
+		   
+		   // Implémentation de FileFilter
+		   public boolean accept(File file){
+		      if (file.isDirectory()) { 
+		         return true; 
+		      } 
+		      
+		      String nomFichier = file.getName().toLowerCase(); 
+		      	return nomFichier.endsWith(extension);
+		   }
+		      public String getDescription(){
+		      return description;
+		   }
+		}
 	
-	
+	public class ComplexFilter extends FileFilter{
 
+		@Override
+		public boolean accept(File f) {
+			// TODO Auto-generated method stub
+			return false;
+		}
+
+		@Override
+		public String getDescription() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+		
+	}
 }
